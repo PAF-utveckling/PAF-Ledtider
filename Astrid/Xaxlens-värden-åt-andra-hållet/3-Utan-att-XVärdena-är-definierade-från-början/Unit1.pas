878c059a12890d61987b251fe799679a315011c7
astridsvanfeldt <astridsvanfeldt@hotmail.com>
2016-07-04 17:40:08 +0200

    Utan att X-värdena är def från början
    När grafens x-värden definieras i efterhand så kommer inte
    värdena/staplarna upp i grafen direkt. När "Enabled := True" är inställt
    på grafen och man flyttar runt den lite, när man kör programmet, så
    kommer alla 14 staplar upp dock. Men då blir axlarna osynkade och det
    ska ju inte vara nödvändigt att man ska behöva flytta grafen manuellt
    för att värdena ska komma upp...

---
+++ Astrid/Xaxlens-värden-åt-andra-hållet/3-Utan-att-XVärdena-är-definierade-från-början/Unit1.pas
@@ -0,0 +1,69 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.TMSChart,
+  FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Chart: TTMSFMXChart;
+    Button1: TButton;
+    Chart2: TTMSFMXChart;
+    procedure InsertToChart;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  InsertToChart;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Chart.BeginUpdate;
+  Chart2.BeginUpdate;
+  Chart.Series[0].MaxX := -0.5;
+  Chart.Series[0].MinX := -15;
+  Chart2.Series[0].MinY := -Chart.Series[0].MaxX-1;
+  Chart2.Series[0].MaxY := -Chart.Series[0].MinX/1.095; // Eftersom den automatiskt lägger till 9,5% till det värde man väljer
+  Chart2.Height := Chart.Width;
+  Chart.EndUpdate;
+  Chart2.EndUpdate;
+end;
+
+procedure Tform1.InsertToChart;
+var
+  i : Integer;
+begin
+  with Chart do
+  begin
+    BeginUpdate;
+    for i := 0 to 14 do
+    begin
+      with Series[0] do
+      begin
+        Points[i].YValue := 2+random(8);
+        Points[i].XValue := -i;
+      end;
+    end;
+    EndUpdate;
+  end;
+end;
+
+end.
